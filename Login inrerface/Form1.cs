8ddf82786392d73877a56b8c2c27c6f850679264
unknown <gfram@hotmail.com>
2019-09-22 13:37:53 +0100

    first build

---
+++ Login inrerface/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_inrerface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var Exit = MessageBox.Show("Are you sure you want to leave the application?", "Are You Sure?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            {
+                if (Exit == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
